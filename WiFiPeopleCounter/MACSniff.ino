51655190c7c4d10d68c697e381f25cc85525d0bf
Ferran <ferri.fc@gmail.com>
2019-11-09 21:03:15 +0100

    arduino ino ESP8266 WiFi People Counter

---
+++ WiFiPeopleCounter/MACSniff.ino
@@ -0,0 +1,116 @@
+// by Ray Burnette 20161013 compiled on Linux 16.3 using Arduino 1.6.12
+// hacked by Kosme 20170520 compiled on Ubuntu 14.04 using Arduino 1.6.11
+// sending MAC data through MQTT by Ferri 20190921 Ubuntu 19.04 & Arduino 1.8.9
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "./functions.h"
+
+#define disable 0
+#define enable  1
+unsigned int channel = 1;
+
+unsigned long lastupload;
+unsigned long timer;
+
+//const char* ssid     = "";         // The SSID (name) of the Wi-Fi network you want to connect to
+//const char* password = "";
+const char* ssid     = "";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "";
+
+const char* mqttServer = "";
+const int mqttPort = 1883;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println(F("MAC Sniffer and MQTT client logger by FerriTheMaker https://github.com/ferrithemaker/esp8266-wifi-people-counter"));
+  Serial.println(F("Based on ESP8266 enhanced sniffer by Kosme https://github.com/kosme"));
+  enablesniffer();
+  lastupload = millis();
+}
+
+void loop() {
+  Serial.print("TIME REMAINING: ");
+  Serial.println(300000 - (timer - lastupload));
+  timer = millis();
+  if (sniffing == false || timer - lastupload > 300000) { // set a time limit to send the data to MQTT server (5 min)
+    sendMQTTdata();
+    lastupload = millis(); 
+    sniffing = true;
+  }
+  if (sniffing == true) {
+    channel = 1;
+    wifi_set_channel(channel);
+    while (true) {
+      nothing_new++;                          // Array is not finite, check bounds and adjust if required
+      if (nothing_new > 100) {
+        nothing_new = 0;
+        channel++;
+        if (channel == 15) break;             // Only scan channels 1 to 14
+        wifi_set_channel(channel);
+      }
+      delay(1);  // critical processing timeslice for NONOS SDK! No delay(0) yield()
+    }
+  }
+}
+
+
+void enablesniffer() {
+  wifi_set_opmode(STATION_MODE);            // Promiscuous works only with station mode
+  wifi_set_channel(channel);
+  wifi_promiscuous_enable(disable);
+  wifi_set_promiscuous_rx_cb(promisc_cb);   // Set up promiscuous callback
+  wifi_promiscuous_enable(enable);
+}
+
+void sendMQTTdata() {
+  wifi_promiscuous_enable(disable);
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid); 
+  Serial.println(" ...");
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++i); 
+    Serial.print(' ');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+  
+  client.setServer(mqttServer, mqttPort);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+  for (int i=0;i<MAXlist;i++) {
+          char strMAC[11]= {lastMACs[i][0],lastMACs[i][1],lastMACs[i][2],lastMACs[i][3],lastMACs[i][4],lastMACs[i][5],lastMACs[i][6],lastMACs[i][7],lastMACs[i][8],lastMACs[i][9],'\0'};
+          if (strMAC!="0000000000") { // if MAC is all 0's skip
+            client.publish("esp/sniffer", strMAC);
+            delay(200);
+          }
+          for (int i2=0;i2<10;i2++) lastMACs[i][i2]=0x00; // clean the array (fill with 0's)
+  }
+  MACindex = 0;
+  // reenable sniffing
+  enablesniffer();
+}
---
+++ WiFiPeopleCounter/functions.h
@@ -0,0 +1,120 @@
+// This-->tab == "functions.h"
+
+// Expose Espressif SDK functionality
+extern "C" {
+#include "user_interface.h"
+  typedef void (*freedom_outside_cb_t)(uint8 status);
+  int  wifi_register_send_pkt_freedom_cb(freedom_outside_cb_t cb);
+  void wifi_unregister_send_pkt_freedom_cb(void);
+  int  wifi_send_pkt_freedom(uint8 *buf, int len, bool sys_seq);
+}
+
+#include <ESP8266WiFi.h>
+#include "./structures.h"
+
+#define MAX_APS_TRACKED 100
+#define MAX_CLIENTS_TRACKED 200
+
+int aps_known_count = 0;                                  // Number of known APs
+int nothing_new = 0;
+int clients_known_count = 0;                              // Number of known CLIENTs
+
+void promisc_cb(uint8_t *buf, uint16_t len)
+{
+  signed potencia;
+  if (len == 12) {
+    struct RxControl *sniffer = (struct RxControl*) buf;
+    potencia = sniffer->rssi;
+  } else if (len == 128) {
+    struct sniffer_buf2 *sniffer = (struct sniffer_buf2*) buf;
+    struct beaconinfo beacon = parse_beacon(sniffer->buf, 112, sniffer->rx_ctrl.rssi);
+    potencia = sniffer->rx_ctrl.rssi;
+  } else {
+    struct sniffer_buf *sniffer = (struct sniffer_buf*) buf;
+    potencia = sniffer->rx_ctrl.rssi;
+  }
+  char currentMAC[10];
+  
+  // Position 12 in the array is where the packet type number is located
+  // For info on the different packet type numbers check:
+  // https://stackoverflow.com/questions/12407145/interpreting-frame-control-bytes-in-802-11-wireshark-trace
+  // https://supportforums.cisco.com/document/52391/80211-frames-starter-guide-learn-wireless-sniffer-traces
+  // https://ilovewifi.blogspot.mx/2012/07/80211-frame-types.html
+
+  
+  if((buf[12]==0x88)||(buf[12]==0x40)||(buf[12]==0x94)||(buf[12]==0xa4)||(buf[12]==0xb4)||(buf[12]==0x08))
+  {
+    //Serial.printf("%02x\n",buf[12]);
+    // if(buf[12]==0x40) Serial.printf("Disconnected: ");
+    // if(buf[12]==0x08) Serial.printf("Data: ");
+    // if(buf[12]==0x88) Serial.printf("QOS: ");
+    // Origin MAC address starts at byte 22
+    // Print MAC address
+
+    // convert *buf (only MAC) to string
+
+    sprintf(currentMAC,"%02x%02x%02x%02x%02x",buf[22],buf[23],buf[24],buf[25],buf[26]);
+    int foundMAC = 0;
+    //Serial.print("Current MAC:");
+    //Serial.println(currentMAC);
+    for (int i=0;i<MAXlist;i++) {
+      if (currentMAC[0] == lastMACs[i][0] && currentMAC[1] == lastMACs[i][1] && currentMAC[2] == lastMACs[i][2] && currentMAC[3] == lastMACs[i][3] && currentMAC[4] == lastMACs[i][4] &&
+          currentMAC[5] == lastMACs[i][5] && currentMAC[6] == lastMACs[i][6] && currentMAC[7] == lastMACs[i][7] && currentMAC[8] == lastMACs[i][8] && currentMAC[9] == lastMACs[i][9]) 
+        {
+        foundMAC = 1;
+        Serial.print("FOUND RECORDED MAC AT ");
+        Serial.println(i);
+        break;
+      }
+    }
+    if (!foundMAC && int8_t(buf[0])>SIGNAL_THRESHOLD) {
+      strncpy(lastMACs[MACindex],currentMAC,10);
+      Serial.println("NEW MAC WITH GOOD SIGNAL DETECTED!");
+      MACindex++;
+    }
+    if (MACindex == MAXlist) {
+      for (int i=0;i<MAXlist;i++) {
+        for (int i2=0;i2<10;i2++) {
+          Serial.print(lastMACs[i][i2]);
+        }
+        Serial.println();
+      }
+      Serial.println("----------------");
+      MACindex = 0;
+      // sending data
+      sniffing = false;
+    }  
+   /*for(int i=0;i<5;i++) {
+      Serial.printf("%02x:",buf[22+i]);
+    }
+    //Serial.printf("%02x  ",buf[22+5]);
+    // Signal strength is in byte 0
+    //Serial.printf("%i\n",int8_t(buf[0]));
+    */
+    // Enable this lines if you want to scan for a specific MAC address
+    // Specify desired MAC address on line 10 of structures.h
+    int same = 1;
+    for(int i=0;i<6;i++)
+    {
+      if(buf[22+i]!=desired[i])
+      {
+        same=0;
+        break;
+      }
+    }
+    if(same)
+    {
+      //Serial.println("Trobat!");
+    }
+    //different device
+    else
+    {
+      //Serial.println(currentMAC);
+    }
+  }
+  //Different packet type numbers
+  else
+  {
+
+  }
+}
