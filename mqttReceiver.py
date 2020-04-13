e4d5bcd7bc52faca6518be98f7b56672c5e0f320
Ubuntu <ubuntu@makeit.cat>
2020-04-13 19:04:25 +0200

    debug and log added, minor changes on db

--- mqttReceiver.py
+++ mqttReceiver.py
@@ -2,60 +2,75 @@
 from influxdb import InfluxDBClient
 import paho.mqtt.client as mqtt
 import time
+import datetime
+import snifferconfig as cfg
 
 def on_message(client,userdata,message):
 	mac = str(message.payload.decode("utf-8"))
-	print("message received: " , mac)
 	if mac != "":
-		last15mResults = influxclient.query('SELECT total FROM traffic_accounting WHERE mac = \''+mac+'\' and time > now() - 15m;')
+		last15mResults = influxclient.query('SELECT activity FROM traffic_accounting WHERE mac = \''+mac+'\' and time > now() - 15m;')
+		#print('SELECT total FROM traffic_accounting WHERE mac = \''+mac+'\' and time > now() - 1h;')
 		last15mPoints = last15mResults.get_points()
 		last15mPoints = list(last15mPoints)
-		halfDayResults = influxclient.query('SELECT total FROM traffic_accounting WHERE mac = \''+mac+'\' and time > now() - 12h;')
-		halfDayPoints = halfDayResults.get_points()
-		halfDayPoints = list(halfDayPoints)
-		allResults = influxclient.query('SELECT total FROM traffic_accounting WHERE mac = \''+mac+'\';')
+		#print(len(points))
+		twoHoursResults = influxclient.query('SELECT activity FROM traffic_accounting WHERE mac = \''+mac+'\' and time > now() - 2h;')
+		twoHoursPoints = twoHoursResults.get_points()
+		twoHoursPoints = list(twoHoursPoints)
+		allResults = influxclient.query('SELECT activity FROM traffic_accounting WHERE mac = \''+mac+'\';')
 		allPoints = allResults.get_points()
 		allPoints = list(allPoints)
 		lenAllPoints = len(allPoints)
-		if len(last15mPoints) == 0 and len(halfDayPoints) > 6:
-			json_insert = [ { "measurement" : "traffic_accounting", "tags" : { "mac" : mac, "permanent" : "yes" }, "fields" : { "total" : 1, "accumulated" : lenAllPoints } } ]
+		if len(last15mPoints) == 0 and len(twoHoursPoints) > 6:
+			json_insert = [ { "measurement" : "traffic_accounting", "tags" : { "mac" : mac, "permanent" : "yes" }, "fields" : { "activity" : 1, "total_activity" : lenAllPoints } } ]
 			influxclient.write_points(json_insert)
-		if len(last15mPoints) == 0 and len(halfDayPoints) <= 6:
-                        json_insert = [ { "measurement" : "traffic_accounting", "tags" : { "mac" : mac, "permanent" : "no" }, "fields" : { "total" : 1, "accumulated" : lenAllPoints } } ]
-                        influxclient.write_points(json_insert)
+			if log:
+				logfile.write("%s,PERMANENT,%s\r\n" %(mac,datetime.datetime.now()))
+			if debug:
+				print("MAC Received:",mac,"ADDED AS PERMANENT at",datetime.datetime.now())
+		elif len(last15mPoints) == 0 and len(twoHoursPoints) <= 6:
+			json_insert = [ { "measurement" : "traffic_accounting", "tags" : { "mac" : mac, "permanent" : "no" }, "fields" : { "activity" : 1, "total_activity" : lenAllPoints } } ]
+			influxclient.write_points(json_insert)
+			if log:
+                                logfile.write("%s,NOT PERMANENT,%s\r\n" %(mac,datetime.datetime.now()))
+			if debug:
+				print("MAC Received:",mac,"ADDED AS NOT PERMANENT at",datetime.datetime.now())
+		else:
+			if log:
+                                logfile.write("%s,NOT ADDED (TOO SOON),%s\r\n" %(mac,datetime.datetime.now()))
+			if debug:
+				print("MAC Received:",mac,"NOT ADDED BECAUSE RECENT PREVIOUS PRESENCE DETECTED < 15M at",datetime.datetime.now())
 
-# MQTT server
+debug = True
+log = True
 
-broker_address="localhost"
-broker_port=1883
-broker_user=""
-broker_password=""
+if log:
+	logfile = open("log.csv", "a")
+
 
 # Influx setup
-
-influxclient = InfluxDBClient(host='localhost', port=8086)
-influxclient.switch_database('sniffer')
+influxclient = InfluxDBClient(host=cfg.db_address, port=cfg.db_port)
+influxclient.switch_database(cfg.db_name)
 
 
 #print("creating new instance")
-
 mqttclient = mqtt.Client("mqttsniffer") #create new instance
-mqttclient.username_pw_set(broker_user,broker_password)
+mqttclient.username_pw_set(cfg.broker_user,cfg.broker_password)
 
 #print("connecting to broker")
-
-mqttclient.connect(broker_address,port=broker_port) #connect to broker
+mqttclient.connect(cfg.broker_address,port=cfg.broker_port) #connect to broker
 mqttclient.loop_start()
 
 #print("Subscribing to topic")
-
-mqttclient.subscribe("esp/sniffer")
+mqttclient.subscribe(cfg.broker_topic)
 mqttclient.on_message=on_message        #attach function to callback
 
 try:
 	while True:
 		time.sleep(1)
+
 except KeyboardInterrupt:
 	print("bye")
+	if log:
+		logfile.close()
 	mqttclient.disconnect()
 	mqttclient.loop_stop()
---
+++ snifferconfig_template.py
@@ -0,0 +1,8 @@
+broker_address="localhost" 
+broker_port=1883
+broker_user=""
+broker_password=""
+broker_topic="esp/sniffer"
+db_address="localhost"
+db_port=8086
+db_name="sniffer"
